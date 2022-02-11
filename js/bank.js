12837d4c0cce52ebdc1b37e2cad33725489c3863
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-11 11:43:53 +0600

    recap

---
+++ js/bank.js
@@ -0,0 +1,45 @@
+// get deposit-btn 
+document.getElementById('deposit-btn').addEventListener('click', function () {
+    const inputAmount = document.getElementById('deposit-input');
+    const depositAmount = inputAmount.value;
+    // console.log(depositAmount)
+
+    //total deposit calculation 
+    const totalDeposit = document.getElementById('total-deposit');
+    const previousDepositAmount = totalDeposit.innerText;
+    const newDepositAmount = parseFloat(previousDepositAmount) + parseFloat(depositAmount);
+
+    totalDeposit.innerText = newDepositAmount;
+
+    // total balance calculation 
+    const totalBalance = document.getElementById('total-balance');
+    const previousBalance = totalBalance.innerText;
+    const newTotalBanlance = parseFloat(previousBalance) + parseFloat(depositAmount);
+    totalBalance.innerText = newTotalBanlance;
+
+    //remove value 
+    inputAmount.value = '';
+})
+
+// get withdraw btn 
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmount = withdrawInput.value;
+    // console.log(withdrawAmount);
+
+    //totalWithdraw 
+    const totalWithdraw = document.getElementById('total-withdraw');
+    const previousWithdraw = totalWithdraw.innerText;
+    const newTotalWithdraw = parseFloat(previousWithdraw) + parseFloat(withdrawAmount);
+    totalWithdraw.innerText = newTotalWithdraw;
+
+    // total balance calculation after withdraw  
+    const totalBalance = document.getElementById('total-balance');
+    const previousBalance = totalBalance.innerText;
+    const newTotalBanlance = parseFloat(previousBalance) - parseFloat(withdrawAmount);
+    totalBalance.innerText = newTotalBanlance;
+
+    withdrawInput.value = ''
+})
+
+
