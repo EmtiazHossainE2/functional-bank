7dd50025cab8802bef1643136077175494368aef
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-11 19:02:30 +0600

    functional bank

--- js/bank.js
+++ js/bank.js
@@ -23,12 +23,6 @@ function totalCashInOut(inputField, inputAmount) {
 document.getElementById('deposit-btn').addEventListener('click', function () {
     const depositAmount = getInputValue('deposit-input');
 
-    //total deposit calculation 
-    /* const totalDeposit = document.getElementById('total-deposit');
-    const previousDepositAmount = totalDeposit.innerText;
-    const newDepositAmount = parseFloat(previousDepositAmount) + depositAmount;
-
-    totalDeposit.innerText = newDepositAmount; */
     totalCashInOut('total-deposit', depositAmount)
 
     // total balance calculation 
@@ -43,11 +37,6 @@ document.getElementById('deposit-btn').addEventListener('click', function () {
 document.getElementById('withdraw-btn').addEventListener('click', function () {
     const withdrawAmount = getInputValue('withdraw-input');
 
-    //totalWithdraw 
-    /* const totalWithdraw = document.getElementById('total-withdraw');
-    const previousWithdraw = totalWithdraw.innerText;
-    const newTotalWithdraw = parseFloat(previousWithdraw) + withdrawAmount;
-    totalWithdraw.innerText = newTotalWithdraw; */
     totalCashInOut('total-withdraw', withdrawAmount)
 
     // total balance calculation after withdraw  
@@ -58,52 +47,3 @@ document.getElementById('withdraw-btn').addEventListener('click', function () {
 
 })
 
-
-/* 
-// Recap Module 26  
-// get deposit-btn 
-document.getElementById('deposit-btn').addEventListener('click', function () {
-    const inputAmount = document.getElementById('deposit-input');
-    const depositAmount = inputAmount.value;
-    // console.log(depositAmount)
-
-    //total deposit calculation 
-    const totalDeposit = document.getElementById('total-deposit');
-    const previousDepositAmount = totalDeposit.innerText;
-    const newDepositAmount = parseFloat(previousDepositAmount) + parseFloat(depositAmount);
-
-    totalDeposit.innerText = newDepositAmount;
-
-    // total balance calculation 
-    const totalBalance = document.getElementById('total-balance');
-    const previousBalance = totalBalance.innerText;
-    const newTotalBanlance = parseFloat(previousBalance) + parseFloat(depositAmount);
-    totalBalance.innerText = newTotalBanlance;
-
-    //remove value 
-    inputAmount.value = '';
-})
-
-// get withdraw btn 
-document.getElementById('withdraw-btn').addEventListener('click', function () {
-    const withdrawInput = document.getElementById('withdraw-input');
-    const withdrawAmount = withdrawInput.value;
-    // console.log(withdrawAmount);
-
-    //totalWithdraw 
-    const totalWithdraw = document.getElementById('total-withdraw');
-    const previousWithdraw = totalWithdraw.innerText;
-    const newTotalWithdraw = parseFloat(previousWithdraw) + parseFloat(withdrawAmount);
-    totalWithdraw.innerText = newTotalWithdraw;
-
-    // total balance calculation after withdraw  
-    const totalBalance = document.getElementById('total-balance');
-    const previousBalance = totalBalance.innerText;
-    const newTotalBanlance = parseFloat(previousBalance) - parseFloat(withdrawAmount);
-    totalBalance.innerText = newTotalBanlance;
-
-    withdrawInput.value = ''
-})
-
-
-*/
